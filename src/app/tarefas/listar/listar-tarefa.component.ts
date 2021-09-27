50a70fac6b091fbc635a95da8534b16d1a075096
BrunoTravassos <brunodefrontin@gmail.com>
2021-09-27 18:01:54 -0300

    commit add livro remoto

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa[]; // vai p/ o html
 
-  ngOnInit(): void {
+  constructor(private tarefaService: TarefaService) { }
+
+  ngOnInit() {
+    this.tarefas = this.listarTodos();
+    this.tarefas = [
+      new Tarefa(1,'Tarefa 01', false),
+      new Tarefa(2,'Tarefa 02', false)
+    ]
+  }
+
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
   }
 
 }
