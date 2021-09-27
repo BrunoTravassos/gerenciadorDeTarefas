de93277ef7c843f34b23186d5575590507308928
BrunoTravassos <brunodefrontin@gmail.com>
2021-09-27 11:03:15 -0300

    commit tratamento de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,2 +1,12 @@
 import { NgModule } from "@angular/core";
 import {Routes, RouterModule } from "@angular/router";
+
+export const router: Routes = [];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(router)],//tratamento de rotas
+  exports: [RouterModule] //
+})
+
+//modulo de rotas
+export class AppRoutingModule{}
