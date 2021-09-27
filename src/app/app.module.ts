2ccba6064c6eb9dead98804b786fa594f03482f8
BrunoTravassos <brunodefrontin@gmail.com>
2021-09-27 11:33:37 -0300

    commit impor tarefas-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TarefasModule } from './tarefas';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule // import do routingmodule
+    AppRoutingModule, // import do routingmodule
+    TarefasModule // import do tarefasmodule
   ],
   providers: [],
   bootstrap: [AppComponent]
